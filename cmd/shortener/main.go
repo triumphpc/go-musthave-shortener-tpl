d1a55811d9b447b99dfae73a621128793e764b14
Sergey Vrulin <triumph.job@gmail.com>
2021-08-23 16:33:55 +0300

    Revert "first commit"
    This reverts commit abd9c4580011b3a4c6613a06de4270f0e4d4b9ef.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,22 +1,3 @@
 package main
 
-import (
-	"github.com/gorilla/mux"
-	"github.com/triumphpc/go-musthave-shortener-tpl/internal/app/handlers"
-	"github.com/triumphpc/go-musthave-shortener-tpl/internal/app/storage"
-	"log"
-	"net/http"
-)
-
-func main() {
-	// Allocation storage for urls
-	s := make(storage.Storage)
-	// Make Routes
-	rtr := mux.NewRouter()
-	rtr.HandleFunc("/{id:.+}", handlers.Get(s))
-	rtr.HandleFunc("/", handlers.Save(s))
-	http.Handle("/", rtr)
-
-	// Init server
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
+func main() {}
