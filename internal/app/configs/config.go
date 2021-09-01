9e2ffd3afa860765ccc7ca478df2ef08dabbc175
Sergey Vrulin <triumph.job@gmail.com>
2021-09-01 23:39:39 +0300

    inc #5 major v1.1 - fix tests

--- internal/app/configs/config.go
+++ internal/app/configs/config.go
@@ -8,9 +8,13 @@ import (
 // DefaultPort for server
 const DefaultPort = "8080"
 
+// CustomPort for server
+const CustomPort = "9080"
+const DefaultHost = "http://localhost"
+
 // Config project
 type Config struct {
-	BaseURL    string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+	BaseURL    string `env:"BASE_URL" envDefault:"http://localhost"`
 	ServerPort string
 }
 
@@ -23,8 +27,14 @@ func New() Config {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// if is set base url on server
-	cfg.ServerPort = DefaultPort
+
+	if cfg.BaseURL != DefaultHost {
+		cfg.ServerPort = CustomPort
+
+	} else {
+		// if is set base url on server
+		cfg.ServerPort = DefaultPort
+	}
 
 	return cfg
 }
