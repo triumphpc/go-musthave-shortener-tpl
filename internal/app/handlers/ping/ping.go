ec273e5ac3cb60b5369571116e51591c7c1805a7
Sergey Vrulin <triumph.job@gmail.com>
2021-09-19 12:22:42 +0300

    inc #11 ver 1.0.2 - fixe tests

--- internal/app/handlers/ping/ping.go
+++ internal/app/handlers/ping/ping.go
@@ -1,21 +1,18 @@
 package ping
 
 import (
-	"context"
 	"github.com/triumphpc/go-musthave-shortener-tpl/internal/app/helpers/db"
 	"github.com/triumphpc/go-musthave-shortener-tpl/internal/app/logger"
 	"go.uber.org/zap"
 	"net/http"
 )
 
-type Handler struct {
-	ctx context.Context
-}
+type Handler struct{}
 
-func (h Handler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	conn, err := db.Instance()
 	if err == nil {
-		if err := conn.PingContext(h.ctx); err == nil {
+		if err := conn.PingContext(r.Context()); err == nil {
 			w.WriteHeader(http.StatusOK)
 		} else {
 			logger.Info("not connect to db", zap.Error(err))
